7c45bbe4e476e90edd9106484789edb812b3f85b
Brian Joseph <brianjoseph1758@gmail.com>
2025-09-10 23:07:44 -0400

    implemented error handling

--- types_walkthrough.py
+++ types_walkthrough.py
@@ -1,22 +1,33 @@
-name = input("What is your name?")
+name = input("What is your name? ")
+
 
 print(f"Hello {name}")
 
 print("Per the Cleveland Clinc, you should eat 2000 calories a day.")
 
-caloricIntake = int(input("How many calories have you consumed tody?"))
+# caloricIntake = int(input("How many calories have you consumed tody?"))
 
-caloriesNeeded = 2000 - caloricIntake 
+while True:
+    try:
+        caloricIntake = int(input("How many calories have you consumed today? "))
+        break   #  Exit loop if input is valid
+    except ValueError:
+        print(" Invalid input! Please enter a number.")
 
-# print(f"You need to eat another {caloriesNeeded} calories today.")
+caloriesNeeded = 2000 - caloricIntake
 
+if caloriesNeeded > 0:
+    print(f"You need to eat another {caloriesNeeded} calories today.")
+if caloriesNeeded <= 2000:
+    print("You have met or exceeded your 2000 calorie goal today!")
+else :
+    print("you need to eat 2000 calories today")
 # print(name,type(name))
 # print(caloriesNeeded,type(caloriesNeeded))
-
-if caloriesNeeded > 0 :
-    print(f"You need to eat another {caloriesNeeded} calories today.")
-    if caloriesNeeded <= 0 :
-        print(f"You need to eat another {caloriesNeeded} calories today.")
+# if caloriesNeeded > 0 :
+#     print(f"You need to eat another {caloriesNeeded} calories today.")
+#     if caloriesNeeded <= 0 :
+#         print(f"You need to eat 2000 calories today.")
 
 players ={
     "Lebron James" : 23,
